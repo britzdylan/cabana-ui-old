9b3aa3adcb4955f31aca58ebd22649d111dd1747
Dylan Britz <britzdylan@gmail.com>
2023-04-21 12:21:18 +0200

    List

--- elements/index.js
+++ elements/index.js
@@ -3,6 +3,7 @@ const link = require('./link');
 const menu = require('./menu');
 const pill = require('./pill');
 const breadcrumb = require('./breadcrumb');
+const list = require('./list');
 const main = (theme) => {
   return {
     ...btn(theme),
@@ -10,6 +11,7 @@ const main = (theme) => {
     ...menu(theme),
     ...pill(theme),
     ...breadcrumb(theme),
+    ...list(theme),
   };
 };
 module.exports = main;
---
+++ elements/list.js
@@ -0,0 +1,45 @@
+module.exports = (theme) => {
+  const listItem = {
+    fontSize: theme('fontSize.label-sm'),
+    color: 'inherit',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: theme('spacing.2'),
+    '&.featured': {
+      fontWeight: theme('fontWeight.semibold'),
+    },
+  };
+
+  const listStyle = {
+    listStyle: 'none',
+    padding: 0,
+    margin: 0,
+    '& li': {
+      ...listItem,
+      '& .icon, svg': {
+        width: theme('spacing.4'),
+        height: theme('spacing.4'),
+      },
+    },
+
+    '&-large': {
+      '& li': {
+        ...listItem,
+        fontSize: theme('fontSize.label-xl'),
+        '& .icon, svg': {
+          width: theme('spacing.6'),
+          height: theme('spacing.6'),
+        },
+      },
+    },
+  };
+  return {
+    'ul.list': {
+      ...listStyle,
+    },
+    'ol.list': {
+      ...listStyle,
+    },
+  };
+};
